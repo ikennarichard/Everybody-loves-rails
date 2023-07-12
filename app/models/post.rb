60be249b9ff863fbea67fd1a5f5001130fd9d291
Ikenna Richard <oguejioforrichard@gmail.com>
2023-07-12 12:40:47 -1100

    Update models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ApplicationRecord
+  attr_accessor :title, :text
+  validates :title, :text, :presence: true
   belongs_to :user
-  has_many :comments, :likes
+  has_many :comments, foreign_key: 'post_id' 
+  has_many :likes, foreign_key: 'post_id'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
-  has_many :comments, :posts, :likes
+
+  attr_accessor :name, :photo, :bio, :post_counter
+  validates :name, presence: true
+
+  has_many :comments, foreign_key: 'author_id' 
+  has_many :posts, foreign_key: 'author_id'
+  has_many :likes, foreign_key: 'author_id'
 end
