3248496122cf34bbf726464cd4c5be79f193b555
Ikenna Richard <oguejioforrichard@gmail.com>
2023-07-12 15:58:40 -1100

    Fix rubocop issues

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,14 +3,15 @@ class Post < ApplicationRecord
   after_save :update_post_counter
 
   belongs_to :author, class_name: 'User'
-  has_many :comments, foreign_key: 'post_id', dependent: :destroy  
+  has_many :comments, foreign_key: 'post_id', dependent: :destroy
   has_many :likes, foreign_key: 'post_id', dependent: :destroy
 
   def get_recent_comments
     comments.order(created_at: :desc).limit(5)
   end
-  
+
   private
+
   def update_post_counter
     author.increment!(:post_counter)
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   validates :name, presence: true
 
-  has_many :comments, inverse_of: 'author', foreign_key: 'author_id', dependent: :destroy 
-  has_many :posts, inverse_of: 'author', foreign_key: 'author_id', dependent: :destroy 
+  has_many :comments, inverse_of: 'author', foreign_key: 'author_id', dependent: :destroy
+  has_many :posts, inverse_of: 'author', foreign_key: 'author_id', dependent: :destroy
   has_many :likes, inverse_of: 'author', foreign_key: 'author_id', dependent: :destroy
 
   def get_recent_posts
