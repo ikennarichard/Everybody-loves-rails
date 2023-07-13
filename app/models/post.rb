fa1e23c5a8ce1e0b7edf61b07287e8d36760b92d
Ikenna Richard <oguejioforrichard@gmail.com>
2023-07-12 15:31:12 -1100

    Add method for post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,17 @@
 class Post < ApplicationRecord
-  attr_accessor :title, :text
-  validates :title, :text, :presence: true
-  belongs_to :user
-  has_many :comments, foreign_key: 'post_id' 
-  has_many :likes, foreign_key: 'post_id'
+  validates :title, :text, presence: true
+  after_save :update_post_counter
+
+  belongs_to :author, class_name: 'User'
+  has_many :comments, foreign_key: 'post_id', dependent: :destroy  
+  has_many :likes, foreign_key: 'post_id', dependent: :destroy
+
+  def get_recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
+  
+  private
+  def update_post_counter
+    author.increment!(:post_counter)
+  end
 end
