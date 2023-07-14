4a0560d3d5ea1d9acbe6538ed8bbf633c8bfbe20
Ikenna Richard <oguejioforrichard@gmail.com>
2023-07-13 17:40:26 -1100

    Add model validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-  validates :title, :text, presence: true
+  validates :title, presence: true, length: { maximum: 250 }
   after_save :update_post_counter
 
   belongs_to :author, class_name: 'User'
