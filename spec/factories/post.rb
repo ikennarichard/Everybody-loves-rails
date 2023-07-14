b7c2c5f12e4d27f6251757e53b3748c0fb15fb37
Ikenna Richard <oguejioforrichard@gmail.com>
2023-07-13 17:38:05 -1100

    Add stubs for the test

---
+++ spec/factories/post.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  
+  factory :post do
+    title { "Greetings" }
+    text { "Hello world" }
+    comments_counter { 0 }
+    likes_counter { 0 }
+  end
+end
---
+++ spec/factories/user.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  
+  factory :user do
+    name { "Teddy" }
+    photo { "teddy-image.jpg" }
+    bio { "software engineer" }
+    post_counter { 0 }
+  end
+end
