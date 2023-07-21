f9925404a4c5d702e97ef4a5f9679a833ba8a8d3
Ikenna Richard <oguejioforrichard@gmail.com>
2023-07-20 18:20:10 -1100

    Refactor code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,11 @@
 class UsersController < ApplicationController
   def index
-    # @users = User.all
+    @users = User.all
   end
 
   def show
-    #   @user = User.find(params[:id])
-    #   @posts = @user.posts
-    # rescue ActiveRecord::RecordNotFound
-    #   render 'errors/not_found', status: :not_found
+      @user = User.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      render 'errors/not_found', status: :not_found
   end
 end
