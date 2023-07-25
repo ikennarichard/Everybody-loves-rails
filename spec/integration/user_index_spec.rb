39a09530fa059171db4438f8bfcaae9c97b030ae
Ikenna Richard <oguejioforrichard@gmail.com>
2023-07-25 10:18:07 -1100

    Add user index page

--- spec/integration/user_index_spec.rb
+++ spec/integration/user_index_spec.rb
@@ -11,21 +11,21 @@ RSpec.describe 'user index page', type: :feature do
   scenario 'I can see the profile picture for each user' do
     visit users_path
     User.all.each do |_user|
-      expect(page).to have_css('user_image')
+      expect(page).to have_css('.user_image')
     end
   end
 
   scenario 'i can see the number of posts' do
     visit users_path
     User.all.each do |user|
-      expect(page).to have_content(user.posts_counter)
+      expect(page).to have_content(user.post_counter)
     end
   end
 
   scenario 'redirect to user\'s show page' do
     visit users_path
-    user = User.find(101)
-    click_on 'Spencer'
+    user = User.find(2)
+    click_on 'Rachael'
     expect(page).to have_current_path(user_show_path(user.id))
   end
 end
