ebd0ea4aff83fbd45e887ed0f45afab9fd340ba8
Ikenna Richard <oguejioforrichard@gmail.com>
2023-07-25 01:29:22 -1100

    Fix rubocop issues

--- spec/integration/post_spec.rb
+++ spec/integration/post_spec.rb
@@ -4,14 +4,12 @@ RSpec.feature 'User Post Index', type: :feature, js: true do
   before(:all) do
     @user = User.first
   end
-
   scenario 'should display users posts index page' do
     visit postindex_path(@user)
 
     expect(page).to have_selector('.user_image')
     expect(page).to have_content(@user.name)
     expect(page).to have_content("Number of Posts: #{@user.posts.count}")
-
     @user.posts.each do |post|
       expect(page).to have_content(post.title)
       expect(page).to have_content(post.text.truncate(10))
