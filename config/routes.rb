1b1a6ad086268a44f0314e46a90571c3cd1a83e9
skabeo <spencerokyere95@gmail.com>
2023-07-25 07:53:52 +0000

    Modify post show path

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'users#index'
   get '/users/', to: 'users#index'
-  get '/users/:id', to: 'users#show' 
+  get '/users/:id', to: 'users#show', as: "user_show"
   
   get '/users/:user_id/posts', to: 'posts#index', as: 'postindex'
   get '/users/:user_id/posts/new', to: 'posts#new', as: 'new_post'
