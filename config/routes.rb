1cbb708ffdc649b125cd9d07fe7adad23d61ba5e
Ikenna Richard <oguejioforrichard@gmail.com>
2023-07-24 13:04:46 -1100

    Update posts routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get '/users/', to: 'users#index'
   get '/users/:id', to: 'users#show' 
   
-  get '/users/:user_id/:posts', to: 'posts#index'
+  get '/users/:user_id/posts', to: 'posts#index'
   get '/users/:user_id/posts/new', to: 'posts#new', as: 'new_post'
   post '/users/:user_id/posts/create', to: 'posts#create', as: 'create_post'
   get '/users/:user_id/posts/:id', to: 'posts#show', as: 'posts'
