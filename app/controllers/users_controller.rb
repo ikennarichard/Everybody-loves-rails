fcd64750742c49df35d82f414fc4124eed98f9df
Ikenna Richard <oguejioforrichard@gmail.com>
2023-07-26 17:47:30 -1100

    Implement requested changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,4 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!, :configure_permitted_parameters, if: :devise_controller?
-
   def index
     @users = User.all
     flash[:notice] = 'You have successfully logged in.'
@@ -14,14 +12,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-  rescue ActiveRecord::RecordNotFound
+    rescue ActiveRecord::RecordNotFound
     render 'errors/not_found', status: :not_found
-  end
-
-  protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:email])
   end
 end
