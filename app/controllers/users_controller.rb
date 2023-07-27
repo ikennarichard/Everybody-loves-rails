256cfe75b8227a5b16d2682874ff39bbf460fd41
Ikenna Richard <oguejioforrichard@gmail.com>
2023-07-26 17:54:30 -1100

    Fix linters

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    rescue ActiveRecord::RecordNotFound
-      render 'errors/not_found', status: :not_found
+  rescue ActiveRecord::RecordNotFound
+    render 'errors/not_found', status: :not_found
   end
 end
