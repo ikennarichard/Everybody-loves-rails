2321757049a5754de48ffcc32dfdf21dc1906ee6
Ikenna Richard <oguejioforrichard@gmail.com>
2023-07-28 07:17:22 -1100

    Add user sign out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
+  def sign_out_user
+    sign_out current_user
+    redirect_to root_path
+  end
+
   rescue_from CanCan::AccessDenied do | exception |
     redirect_to root_url, alert: exception.message
   end
