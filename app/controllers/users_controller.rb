d8b80505903e3bed9aadb177cb0445487a132571
Ikenna Richard <oguejioforrichard@gmail.com>
2023-07-28 07:25:42 -1100

    Update users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,5 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-  rescue ActiveRecord::RecordNotFound
-    render 'errors/not_found', status: :not_found
   end
 end
