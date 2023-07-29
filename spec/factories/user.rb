934b4e31295fcbabda7c5187e2247bfc102ba8a6
Ikenna Richard <oguejioforrichard@gmail.com>
2023-07-29 03:14:51 -1100

    Update test factories

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,8 +1,12 @@
 FactoryBot.define do
   factory :user do
+    # id { 114 }
+    sequence(:id) { |n| n }
     name { 'Teddy' }
     photo { 'teddy-image.jpg' }
     bio { 'software engineer' }
+    sequence(:email) { |n| "user#{n}@example.com" }
+    password { 'pass-1' }
     post_counter { 0 }
   end
 end
